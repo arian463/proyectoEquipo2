95c3a5b4d51e660450bf9c7430eaaaff4f390bcb
Arturocs160 <kronculd@outlook.com>
2026-02-01 18:27:40 -0600

    Reorganiza el método de registro y login en el modelo de autenticación

--- proyecto/backend/src/models/AuthModel.ts
+++ proyecto/backend/src/models/AuthModel.ts
@@ -1,11 +1,19 @@
 import connection from "@config/config"
 
 class AuthModel {
-    
-    static async register(id: string, nombre: string, email: string, password: string, rol: string) {
+    static async login(email: string) {
         try {
-            await connection.query('INSERT INTO usuarios (id, nombre, email, password, rol) VALUES (?, ?, ?, ?, ?)', [id, nombre, email, password, rol]);
-            const [rows] = await connection.query('SELECT * FROM usuarios WHERE id = ?', [id]);
+            const [rows] = await connection.query('SELECT * FROM users WHERE email = ?', [email]);
+            return rows as any;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async register(id: string, full_name: string, email: string, password: string, role: string) {
+        try {
+            await connection.query('INSERT INTO users (id, full_name, email, password, role) VALUES (?, ?, ?, ?, ?)', [id, full_name, email, password, role]);
+            const [rows] = await connection.query('SELECT * FROM users WHERE id = ?', [id]);
             return rows as any;
         } catch (error) {
             throw error;

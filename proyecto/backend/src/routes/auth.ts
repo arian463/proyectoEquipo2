215cbfac0d9559ff10a3731b94a9650e9642c53d
Arturocs160 <kronculd@outlook.com>
2026-02-01 18:35:06 -0600

    Se agrega nueva ruta para hacer el cierre de sesiom

--- proyecto/backend/src/routes/auth.ts
+++ proyecto/backend/src/routes/auth.ts
@@ -1,9 +1,11 @@
 import AuthController from "@controllers/AuthController";
+import { checkAuth } from "@middlewares/checkAuth";
 import { Router } from "express";
 
 const routerAuth = Router();
 
 routerAuth.post("/login", AuthController.login);
-routerAuth.post("/register", AuthController.register)
+routerAuth.post("/register", AuthController.register);
+routerAuth.post('/logout', checkAuth, AuthController.logout)
 
 export default routerAuth;

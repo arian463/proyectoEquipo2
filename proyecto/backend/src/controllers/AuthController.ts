68330d87c95b050d07b434221c7a3a6711967fd5
Arturocs160 <kronculd@outlook.com>
2026-02-01 18:28:36 -0600

    Refactorizar los métodos de inicio de sesión y registro en AuthService y AuthController para mejorar el manejo de errores y la consistencia.

--- proyecto/backend/src/controllers/AuthController.ts
+++ proyecto/backend/src/controllers/AuthController.ts
@@ -3,11 +3,28 @@ import { Request, Response } from "express";
 
 class AuthController {
 
+    static async login(request: Request, response: Response) {
+        try {
+            const { email, password } = request.body;
+
+            const result = await AuthService.login(email, password);
+
+            if (!result) {
+                response.status(401).json({ message: "Crendenciales inválidas" });
+                return;
+            }
+
+            response.status(200).json({ message: "Login exitoso", data: result })
+        } catch (error) {
+            response.status(500).json({ message: "Error interno del servidor", error });
+        }
+    }
+
     static async register(request: Request, response: Response) {
         try {
-            const { nombre, email, password, rol } = request.body;
+            const { full_name, email, password, role } = request.body;
 
-            const result = await AuthService.register(nombre, email, password, rol);
+            const result = await AuthService.register(full_name, email, password, role);
 
             if (!result) {
                 response.status(401).json({ message: "Crendenciales inválidas" });
--- proyecto/backend/src/services/AuthService.ts
+++ proyecto/backend/src/services/AuthService.ts
@@ -7,17 +7,39 @@ import "dotenv/config"
 
 class AuthService {
 
-    static async register(nombre: string, email: string, password: string, rol: string) {
+    static async login(email: string, password: string) {
+        const user = await AuthModel.login(email);
+
+        if (user.length === 0) {
+            throw new Error("Credenciales inválidas");
+        }
+
+        const { id, full_name, role, password: passwordUser } = user[0];
+
+        const isPasswordValid = await bcrypt.compare(password, passwordUser);
+
+        if (!isPasswordValid) {
+            throw new Error("Credenciales inválidas");
+        }
+
+        const timestamp = Date.now();
+
+        const token = jwt.sign({ id, timestamp }, process.env.JWT_SECRET as string, { expiresIn: "1h" });
+
+        return { token, id, full_name, email, role };
+    }
+
+    static async register(full_name: string, email: string, password: string, role: string) {
         const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
         const id = randomUUID();
 
         try {
-            await AuthModel.register(id, nombre, email, hashedPassword, rol);
+            await AuthModel.register(id, full_name, email, hashedPassword, role);
         } catch (error) {
             throw new Error("Error al registrar el usuario");
         }
 
-        return { id, nombre, email, rol };
+        return { id, full_name, email, role };
     }
 
 }

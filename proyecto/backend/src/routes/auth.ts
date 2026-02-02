55ee401dfe5507386976822fecafeaec489afc01
Arturocs160 <kronculd@outlook.com>
2026-02-01 18:27:13 -0600

    Agrega la ruta de autenticación para el inicio de sesión en el controlador de autenticación

--- proyecto/backend/src/routes/auth.ts
+++ proyecto/backend/src/routes/auth.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 
 const routerAuth = Router();
 
+routerAuth.post("/login", AuthController.login);
 routerAuth.post("/register", AuthController.register)
 
 export default routerAuth;

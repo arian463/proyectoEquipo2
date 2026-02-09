bcc74a8033e1b5c6d892acd087f7d3031739fbfa
Arturocs160 <kronculd@outlook.com>
2026-02-08 20:19:47 -0600

    Agregar validación de datos para el inicio de sesión y registro de usuario en las rutas de autenticación

--- proyecto/backend/src/routes/auth.ts
+++ proyecto/backend/src/routes/auth.ts
@@ -1,11 +1,13 @@
 import AuthController from "@controllers/AuthController";
 import { checkAuth } from "@middlewares/checkAuth";
 import { Router } from "express";
+import { userLoginSchema, userRegisterSchema } from "schemas/userSchemas";
+import { validateData } from "@middlewares/validateData";
 
 const routerAuth = Router();
 
-routerAuth.post("/login", AuthController.login);
-routerAuth.post("/register", AuthController.register);
+routerAuth.post("/login", validateData(userLoginSchema), AuthController.login);
+routerAuth.post("/register", validateData(userRegisterSchema), AuthController.register);
 routerAuth.post('/logout', checkAuth, AuthController.logout)
 
 export default routerAuth;

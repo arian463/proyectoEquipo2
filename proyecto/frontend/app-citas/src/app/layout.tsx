add5464d9d573768f981672da425e81191a61d07
Arturocs160 <kronculd@outlook.com>
2026-02-15 19:29:01 -0600

    Agregar el proveedor NextAuthProvider en el layout para la gestión de sesiones

--- proyecto/frontend/app-citas/src/app/layout.tsx
+++ proyecto/frontend/app-citas/src/app/layout.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import Menu from "../components/Menu";
 import { usePathname } from "next/navigation";
+import { NextAuthProvider } from "@/providers/NextAuthProvider";
 
 export default function RootLayout({
   children,
@@ -12,11 +13,11 @@ export default function RootLayout({
 
   const pathname = usePathname();
 
-  const hideMenuRoutes = ["/register","/login","/client/servicios","/client/citas", "/client/Explorar",
-                          "/admin/dashboard", "/admin/service","/admin/NuevoServicio","/admin/EditService",
-                          "/admin/ListClient","/admin/NewClient","/admin/EditClient","/admin/ListCitas",
-                          "/client/SelectService","/client/date"
-                        ]
+  const hideMenuRoutes = ["/register", "/login", "/client/servicios", "/client/citas", "/client/Explorar",
+    "/admin/dashboard", "/admin/service", "/admin/NuevoServicio", "/admin/EditService",
+    "/admin/ListClient", "/admin/NewClient", "/admin/EditClient", "/admin/ListCitas",
+    "/client/SelectService", "/client/date"
+  ]
 
   return (
     <html lang="es">
@@ -27,12 +28,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900&display=swap"
           rel="stylesheet"
         />
-        
+
       </head>
       <body>
         {!hideMenuRoutes.includes(pathname) && <Menu />}
-        
-        {children}
+
+        <NextAuthProvider>
+          {children}
+        </NextAuthProvider>
       </body>
     </html>
   );
